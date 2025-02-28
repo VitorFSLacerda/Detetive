6fb8afb4d761c046e887d2f1e7005caf580b8db8
Lari <l177154@dac.unicamp.br>
2025-02-28 14:00:05 -0300

    text aux file and main

---
+++ Detetive/beginingText.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let title = """
+██████╗ ███████╗████████╗███████╗████████╗██╗██╗   ██╗███████╗███████╗
+██╔══██╗██╔════╝╚══██╔══╝██╔════╝╚══██╔══╝██║██║   ██║██╔════╝██╔════╝
+██║  ██║█████╗     ██║   █████╗     ██║   ██║██║   ██║█████╗  ███████╗
+██║  ██║██╔══╝     ██║   ██╔══╝     ██║   ██║╚██╗ ██╔╝██╔══╝  ╚════██║
+██████╔╝███████╗   ██║   ███████╗   ██║   ██║ ╚████╔╝ ███████╗███████║
+╚═════╝ ╚══════╝   ╚═╝   ╚══════╝   ╚═╝   ╚═╝  ╚═══╝  ╚══════╝╚══════╝
+"""
+
+let intro = """
+    Você é um detetive experiente, chamado para resolver um caso misterioso em uma chácara isolada, onde um grupo de amigos universitários estava passando o fim de semana. A noite parecia promissora, com jogos, música e bebidas. No entanto, algo terrível aconteceu. Na manhã seguinte, o corpo de André, um estudante de Ciência da Computação, é encontrado boiando na piscina.
+    Agora, sua missão é clara: juntar as pistas, interrogar os amigos de André e descobrir quem é o responsável por sua morte. A verdade está aí fora, mas você precisa de discernimento e habilidade para encontrá-la.
+"""
