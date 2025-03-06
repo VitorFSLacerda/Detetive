0b0021cd6fd61eac471fc64322bcb2d08e5592cf
Vitor <floco.vitor@gmail.com>
2025-03-06 10:00:17 -0300

    Insertion of characters information.

--- Detetive/suspect.swift
+++ Detetive/suspect.swift
@@ -4,11 +4,14 @@ class Suspect: Node {
     let endStory: String
     let alibi: String
     let insight: String
+    let information: String
+    
 
-    init(_ name: String, _ alibi: String, _ endStory: String, _ insight: String) {
+    init(_ name: String, _ alibi: String, _ endStory: String, _ insight: String,  _ information: String) {
         self.alibi = alibi
         self.endStory = endStory
         self.insight = insight
+        self.information = information
         super.init(name: name)
     }
     
